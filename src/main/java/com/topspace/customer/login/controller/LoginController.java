aa0abbb24c0fd4c7a1d3d6fd067e7f81ee97a42e
ht604061765 <15943286270@163.com>
2018-01-16 22:27:37 +0800

    18011601
    18011601

--- src/main/java/com/topspace/customer/login/controller/LoginController.java
+++ src/main/java/com/topspace/customer/login/controller/LoginController.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.swing.JRadioButton;
 
