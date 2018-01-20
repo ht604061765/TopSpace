6f20d1f4736bc18c821eb1a0853f584396da66aa
ht604061765 <15943286270@163.com>
2018-01-20 19:54:45 +0800

    180120
    180120

--- src/main/java/com/topspace/test/controlle/shishiController.java
+++ src/main/java/com/topspace/test/controlle/shishiController.java
@@ -8,6 +8,7 @@ package com.topspace.test.controlle;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -27,6 +28,7 @@ import com.topspace.core.framework.misc.SystemConstant;
 import com.topspace.core.framework.session.SessionUtil;
 import com.topspace.core.utils.Json4Return;
 import com.topspace.core.utils.PKCreator;
+import com.topspace.core.utils.StringUtil;
 import com.topspace.core.utils.URLUtil;
 import com.topspace.customer.login.entity.UserBo;
 import com.topspace.customer.login.entity.UserInfoBo;
@@ -37,6 +39,9 @@ import com.topspace.customer.login.service.LoginService;
 @RequestMapping("/shishi.do")
 public class shishiController extends BaseController{
 	
+	@Resource
+	LoginService LoginService;
+	
     @RequestMapping(params = "p=test")
     @ResponseBody
 	public String checkUser(String gname,String gsex){
@@ -45,21 +50,53 @@ public class shishiController extends BaseController{
     	System.out.println("gname="+gname+";gsex="+gsex);
     	
     	System.out.println("++++++++++++++++++++++");
-//    	String checkUserReturn = "OK";
-//    	UserBo user = LoginService.selectUser(userAccount);
-//    	if (user == null) {
-//    		checkUserReturn = "hasNoUser";
-//		}else{
-//			if (!user.getUserPassword().equals(userPwd)) {
-//	    		checkUserReturn = "invalidPwd";
-//			}
-//		}
-//		Json4Return jr = new Json4Return(true,checkUserReturn);
-//		return jr;
+
+       return null;
+    }
+    
+    @RequestMapping(params = "p=testMvc")
+    @ResponseBody
+	public String testMvc(UserBo ub1){
+    	String aaaString = ub1.getUserAccount();
+    	ub1.setId("aaa");
+		LoginService.insertUser(ub1);
        return null;
     }
    
+
+ @RequestMapping(params = "p=shuuju")
+ @ResponseBody
+ public String shuuju(String userAccount,String userPassword){
+	 Date date = new Date();
+	 UserBo b = new  UserBo();
+	b.setUserAccount(userAccount);
+	b.setUserPassword(userPassword);
+	b.setId(StringUtil.toString(date));
+	 
+	 LoginService.insertUser(b);
+	 return null;
+   }
+ 
+ 
+ @RequestMapping(params = "p=shuju")
+ @ResponseBody
+ public String shuju(UserInfoBo c){
+ Date date = new Date();
+ c.setId(StringUtil.toString(date));
+ c.setCertEmail("1");
+ c.setCertName("2");
+ c.setCertNo("3");
+ c.setCertPhone("4");
+ c.setCertSex("5");
+ c.setCertType("6");
+ c.setDescription("7");
+ c.setHeadPic("8");
+ c.setNickName("9");
+ c.setNote("10");
+ c.setQq("11");
+ c.setUserId("12");
+ c.setWx("13");
+ LoginService.insertUserInfo(c);
+ return null;
+  }
 }
-
-
-
