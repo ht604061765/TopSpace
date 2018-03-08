72c964926154f51ad99220472e63edbe4fa67de3
ht604061765 <15943286270@163.com>
2018-03-08 22:19:46 +0800

    180308
    180308

--- src/main/java/com/topspace/customer/taskjob/refreshBackgroundJob.java
+++ src/main/java/com/topspace/customer/taskjob/refreshBackgroundJob.java
@@ -6,6 +6,7 @@
  */
 package com.topspace.customer.taskjob;
 
+import java.io.Console;
 import java.io.File;
 import java.io.InputStream;
 
@@ -20,13 +21,15 @@ public class refreshBackgroundJob implements Job{
 	@Override
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
+		System.out.println("【更新背景图】开始");
 		// TODO Auto-generated method stub
 		String url = "https://www.dujin.org/sys/bing/1920.php";
         String fileName = "Login_backGround.png";
-        File file = new File("E:/03workspace/TopSpaceMaven/src/main/webapp/static_resources/images/", fileName);
+        File file = new File("E:/03workspace/TopSpace/src/main/webapp/static_resources/images/", fileName);
 
         InputStream inputStream = URLUtil.getInputStreamByGet(url);
          URLUtil.saveData(inputStream, file);
+        System.out.println("【更新背景图】完毕");
 	}
 
 }
